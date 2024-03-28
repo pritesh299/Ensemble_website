a9dfaef7c6c8f6927ef617092ed966cb519ecddb
Priteh Jogdhankar <priteshjogdhankar@gmail.com>
2024-03-28 20:31:46 +0530

    added content part1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import './style.css';
 
 const navIcon = document.getElementById('nav-icon');
 const mobileNav = document.querySelector('.mobile_nav');
+const activitiesNav=document.getElementById("activities_nav")
+const activitiesShow=document.getElementById("activities_show")
+
+activitiesNav.addEventListener("mouseleave",()=>{
+    activitiesShow.style.display='none'
+})
+activitiesNav.addEventListener("mouseenter",()=>{
+    activitiesShow.style.display='flex'
+})
+console.log(activitiesNav,activitiesShow.classList)
 
 navIcon.addEventListener('click', () => {
   mobileNav.classList.toggle('hidden');
@@ -20,26 +30,34 @@ form.addEventListener("submit",(e)=>{
 
 })
  */
+
 function setCurrentDateTime() {
     var now = new Date();
-    var dateTimeString = now.toISOString(); // Format: YYYY-MM-DDTHH:MM:SSZ
+    var dateTimeString = now.toISOString(); 
     document.getElementById("datetime").value = dateTimeString;
   }
 console.log("lol")
 
 let Announcements = [
     {
-        imgLink: "./img/stock.jpg",
+        imgLink: "./img/atom.jpg",
         title: "Paper Presentation on Quantum-based applications",
         subContent: "Paper presentation done by enthusiastic 1st and 2nd year student. Topics given by the convenors.",
         readMoreLink: "#"
     },
     {
                imgLink: "./img/stock.jpg",
-        title: "Paper Presentation on Quantum-based applications",
-        subContent: "Paper presentation done by enthusiastic 1st and 2nd year student. Topics given by the convenors.",
+        title: "Lecture series",
+        subContent: "Introductory lecture series organised by senior students/convenors in various topics in Physics like Statistical Physics, CMP etc.",
         readMoreLink: "#"
-    }
+    },
+    {
+        imgLink: "./img/news.jpg",
+        title: "Newsletters",
+        subContent: "From entanglement to superposition, delve into the mysteries of the quantum realm with engaging discussions and interactive experiments. ",
+        readMoreLink: "#"
+    },
+
 ];
 
 let AnnouncementsContainer = document.getElementById("News_and_Annoucements_container");
@@ -51,19 +69,18 @@ Announcements.map((card) => {
 
     HTMLcard.innerHTML = `
         <div style="transition: transform 0.2s ease-in-out" class="max-w-sm ${count % 2 === 0 ? 'bg-white  border-white  ' : 'bg-black border-white '} border-y  shadow hover:scale-105 ">
-            <a href="#">
-                <img class="" src="${card.imgLink}" alt="" />
-            </a>
-            <div class="p-5 border-y ${count % 2 === 0 ? ' border-black ' : ' border-white '}">
+            <div href="#" style="background-image: url(${card.imgLink});" class="h-[400px] bg-cover  ">
+            </div >
+            <div class="p-5 news_content flex flex-col justify-around border-y ${count % 2 === 0 ? ' border-black ' : ' border-white '}">
+            <div class="py-4 "> 
                 <a href="#">
                     <h6 class="mb-2 text-xl font-bold tracking-tight  ${count % 2 === 0 ? 'text-gray-900  ' : ' text-white'}">${card.title}</h6>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${card.subContent}</p>
-                <a href="${card.readMoreLink}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                </div>
+                <a href="${card.readMoreLink}" class="News_content_button inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
+                 
                 </a>
             </div>
         </div>
